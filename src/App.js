9be7d4c0d2ae13aace56535efc7df34c4f2e5428
Parth More <moreparth105@gmail.com>
2025-08-04 18:27:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './App.css';
+import { v4 as uuid } from 'uuid';
+
+function App() {
+  const [todo, setTodo] = useState('');
+  const [todolist, setTodolist] = useState([]);
+
+  const onTodoInputchange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const onAddTodolist = () => {
+    setTodolist([...todolist, { id: uuid(), todo: todo, isCompleted: false }]);
+    setTodo('');
+  };
+
+  const onDelete = (id) => {
+    const updatedTodoList = todolist.filter(item => item.id !== id);
+    setTodolist(updatedTodoList);
+  };
+
+  const onTodoCheckchange = (id) => {
+    const updatedTodoList = todolist.map(todo =>
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
+    setTodolist(updatedTodoList);
+  };
+
+  return (
+    <div className='App'>
+      <h1>My wishlist</h1>
+      <div>
+        <input
+          placeholder='Add your wishlist...'
+          value={todo}
+          onChange={onTodoInputchange}
+        />
+        <button onClick={onAddTodolist}>Add</button>
+      </div>
+      <div>
+        {
+          todolist.length > 0 &&
+          todolist.map(item => (
+            <div key={item.id}>
+              <label>
+                <input
+                  type='checkbox'
+                  checked={item.isCompleted}
+                  onChange={() => onTodoCheckchange(item.id)}
+                />
+                <span style={{ textDecoration: item.isCompleted ? 'line-through' : 'none' }}>
+                  {item.todo}
+                </span>
+              </label>
+              <button onClick={() => onDelete(item.id)}>Delete</button>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
